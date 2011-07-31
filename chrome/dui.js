d754a19ff1f33e1e7fdae67687bc85c377be595c
jpnance <jpnance@c86da36a-f20b-4c57-808e-3c44fc92181e>
2011-07-31 17:10:09 +0000

    Migrated over to chrome/ most of the rest of the DUI code which puts salaries and contracts on the screen. The state code still needs to be implemented but this is a good initial run.
    Also, of course, migrated over the list of pages we should care about into the manifest.
    
    git-svn-id: file:///srv/svn/dui/trunk@8 c86da36a-f20b-4c57-808e-3c44fc92181e

--- chrome/dui.js
+++ chrome/dui.js
@@ -1,12 +1,158 @@
 $(document).ready(function() {
 	setColors();
-	chrome.extension.sendRequest({'action' : 'fetchDynastyData'}, onDynastyData);
+	loadDynastyData();
 });
+
+var version = 0.1;
+var currentYear = 2010;
+
+var positions = new Array();
+var salaries = new Array();
+var starts = new Array();
+var ends = new Array();
+
+function loadDynastyData() {
+	chrome.extension.sendRequest({'action' : 'fetchDynastyData'}, onDynastyData);
+}
 
 function onDynastyData(data) {
 	console.log("onDynastyData: ");
 	if (data) {
-		console.log("We've got something.");
+		var parser = new DOMParser();
+		var parsedXml = parser.parseFromString(data, "text/xml");
+
+		var latestVersionString = parsedXml.getElementsByTagName("version")[0].firstChild.nodeValue;
+		var latestVersionFloat = parseFloat(latestVersionString);
+
+		if (version < latestVersionFloat) {
+			var upgrade = confirm("There's a newer version of DUI available.\nWould you like to upgrade to the latest version?");
+
+			if (upgrade) {
+				window.location = "http://thedynastyleague.com/dui/dui-latest.user.js";
+			}
+		}
+
+		$("div#playerTableHeader li").bind("click", loadDynastyData);
+		$("ul.lineupsNav li.lineupsNavItemOff div").bind("click", loadDynastyData);
+		$("td.playertableStat a").bind("click", loadDynastyData);
+		$("tr.tableSubHead td.playertableData a").bind("click", loadDynastyData);
+		$("div.paginationNav a").bind("click", loadDynastyData);
+		$("ul.filterToolsOptionSet li a").bind("click", loadDynastyData);
+
+		// set up maps of player IDs to salaries and player IDs to contracts
+		var draftedXml = parsedXml.getElementsByTagName("drafted");
+		if (draftedXml != null) {
+			var players = draftedXml[0].getElementsByTagName("player");
+
+			var playerId, contract, salary, start, end;
+
+			for (var i = 0; i < players.length; i++) {
+				playerId = null;
+				contract = null;
+				salary = null;
+				start = null;
+				end = null;
+
+				playerIdNode = players[i].getElementsByTagName("id");
+
+				if (playerIdNode.length > 0) {
+					playerId = playerIdNode[0].firstChild.nodeValue;
+
+					positionNode = players[i].getElementsByTagName("position");
+					contractNode = players[i].getElementsByTagName("contract");
+
+					if (positionNode.length > 0) {
+						position = positionNode[0].firstChild.nodeValue;
+						positions[playerId] = position;
+					}
+
+					if (contractNode.length > 0) {
+						salaryNode = contractNode[0].getElementsByTagName("salary");
+						startNode = contractNode[0].getElementsByTagName("start");
+						endNode = contractNode[0].getElementsByTagName("end");
+
+						if (salaryNode.length > 0) { salary = salaryNode[0].firstChild.nodeValue; }
+						if (startNode.length > 0) { start = startNode[0].firstChild.nodeValue; }
+						if (endNode.length > 0) { end = endNode[0].firstChild.nodeValue; }
+
+						salaries[playerId] = salary;
+						starts[playerId] = start;
+						ends[playerId] = end;
+					}
+				}
+			}
+		}
+
+		$("*[id^=playername]").each(
+			function() {
+				var playerId = this.id.match(/playername_(\d+)/)[1];
+				var salaryDisplay, contractDisplay, contractTip;
+				var salary, term, finalYear, inYear, yearsLeft, contractTip;
+
+				if (salaries[playerId] > 0) {
+					salaryDisplay = "<strong>$" + salaries[playerId] + "</strong>";
+					salary = salaries[playerId];
+				}
+				else {
+					salaryDisplay = "<em>Free Agent</em>";
+				}
+
+				//if (starts[playerId] == null || starts[playerId] == "FA" || ends[playerId] == null) {
+				if (salaries[playerId] == null || salaries[playerId] == 0) {
+					contractDisplay = "";
+					contractTip = "This player is currently a free agent.";
+				}
+				else {
+					contractDisplay = " - ";
+					contractTip = "";
+
+					if (ends[playerId] != "Not Yet Signed") {
+						var startYear = starts[playerId];
+						var endYear = ends[playerId];
+
+						finalYear = endYear;
+
+						if (startYear == "FA") {
+							startYear = endYear;
+							finalYear = "RFA";
+						}
+
+						inYear = currentYear - startYear + 1;
+						contractYears = endYear - startYear + 1;
+
+						contractDisplay += finalYear + " <em>(" + inYear + "/" + contractYears + ")</em>";
+
+						var tempSalary = salary;
+						var forYear = currentYear;
+						contractTip = "Relief: ";
+
+						for (var i = inYear; i <= contractYears; i++) {
+							tempSalary = salary - Math.ceil(salary * (0.6 / Math.pow(2, i - 1)));
+							forYear = currentYear + i - inYear;
+							contractTip += "$" + tempSalary + " in " + forYear;
+
+							if (contractYears > i) {
+								contractTip += ", ";
+							}
+						}
+					}
+					else {
+						contractDisplay += "<em>Not Yet Signed</em>";
+						contractTip = "This player has not yet signed with his team.";
+					}
+				}
+
+				$("#" + this.id)
+				.append(
+					$("<div />")
+					.append(salaryDisplay)
+					.append(contractDisplay)
+					.attr("title", contractTip)
+				);
+
+				//$("td#" + this.id).parent().css("background-color", positionColors[positions[playerId]]);
+			}
+		);
 	}
 	else {
 		console.log("We've got nothing.");
