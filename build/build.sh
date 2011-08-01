e457f9a5733e065e00d512a42725adf4d2ca1ca0
jpnance <jpnance@c86da36a-f20b-4c57-808e-3c44fc92181e>
2011-08-01 06:29:09 +0000

    Started fleshing out the first notion of common code within the build script. This involved moving a lot of chrome/dui.js and the jQuery file into common/.
    git-svn-id: file:///srv/svn/dui/trunk@11 c86da36a-f20b-4c57-808e-3c44fc92181e

--- build/build.sh
+++ build/build.sh
@@ -5,4 +5,13 @@ then
 	mkdir chrome
 fi
 
-cp ../chrome/dui.js chrome/
+cp ../chrome/manifest.json chrome/
+cp ../chrome/background.html chrome/
+cat > chrome/dui.js << EOF
+`cat ../common/jquery-1.6.2.min.js`
+
+`cat ../chrome/dui.js`
+
+`cat ../common/dui.js`
+
+EOF
