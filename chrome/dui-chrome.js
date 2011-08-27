e3d0b622c1465ab42c3d408f0c16575bb72bc4b9
jpnance <jpnance@c86da36a-f20b-4c57-808e-3c44fc92181e>
2011-08-27 23:09:26 +0000

    Accidentally omitted this from the last commit. It's just explicitly putting the CRX update link in the CRX-specific JS file.
    git-svn-id: file:///srv/svn/dui/trunk@23 c86da36a-f20b-4c57-808e-3c44fc92181e

--- chrome/dui-chrome.js
+++ chrome/dui-chrome.js
@@ -1,6 +1,7 @@
 $(document).ready(loadDynastyData);
 
 var showColors = true;
+var upgradeLink = "http://thedynastyleague.com/dui/dui-stable.crx";
 
 function saveColorSetting() {
 	// TODO: Learn how to do this.
