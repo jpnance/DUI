8dc47501bd1100ccc8db3249deee8d970856566b
Patrick Nance <jpnance@gmail.com>
2017-10-25 11:27:19 -0700

    Caching data so we only make a new network request every five minutes.

--- background.js
+++ background.js
@@ -1,40 +1,67 @@
-['showColors', 'showContracts', 'fba', 'ffl'].forEach(function(key) {
-	if (localStorage[key] != null) {
+var defaults = {
+	'showColors': undefined,
+	'showContracts': undefined,
+	'fba': undefined,
+	'ffl': undefined,
+
+	'ffl-contracts': 'true',
+	'ffl-colors': 'true',
+	'ffl-data': null,
+	'ffl-last-fetched': null,
+
+	'fba-contracts': 'true',
+	'fba-colors': 'true',
+	'fba-data': null,
+	'fba-last-fetched': null
+};
+
+Object.keys(defaults).forEach(function(key) {
+	if (defaults[key] === undefined) {
 		delete localStorage[key];
 	}
-});
-
-['ffl-contracts', 'ffl-colors', 'fba-contracts', 'fba-colors'].forEach(function(key) {
-	if (localStorage[key] == null) {
-		localStorage[key] = 'true';
+	else {
+		if (localStorage[key] === undefined) {
+			localStorage[key] = defaults[key];
+		}
 	}
 });
 
 function fetchDynastyData(context, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function(data) {
-		if (xhr.readyState == 4) {
-			if (xhr.status == 200) {
-				var data = xhr.responseText;
-				callback(data);
+	if (needsNewData(context)) {
+		//console.log('fetching new ' + context + ' data');
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function(data) {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					var data = xhr.responseText;
+
+					localStorage[context + '-last-fetched'] = (new Date()).toString();
+					localStorage[context + '-data'] = data;
+
+					callback(data);
+				}
+				else {
+					callback(null);
+				}
 			}
-			else {
-				callback(null);
-			}
+		};
+
+		var url = '';
+
+		if (context == 'ffl') {
+			url = 'http://thedynastyleague.com/data/data.xml?_=' + Math.random();
 		}
-	};
+		else if (context == 'fba') {
+			url = 'http://thedynastyleague.com/data/colbys.xml?_=' + Math.random();
+		}
 
-	var url = '';
-
-	if (context == 'ffl') {
-		url = 'http://thedynastyleague.com/data/data.xml?_=' + Math.random();
+		xhr.open('GET', url, true);
+		xhr.send();
 	}
-	else if (context == 'fba') {
-		url = 'http://thedynastyleague.com/data/colbys.xml?_=' + Math.random();
+	else {
+		//console.log('using cached ' + context + ' data');
+		callback(localStorage[context + '-data']);
 	}
-
-	xhr.open('GET', url, true);
-	xhr.send();
 };
 
 function onMessage(request, sender, callback) {
@@ -56,5 +83,18 @@ function onMessage(request, sender, callback) {
 
 	return true;
 };
+
+function needsNewData(context) {
+	if (localStorage[context + '-last-fetched']) {
+		var now = new Date();
+		var lastFetched = new Date(localStorage[context + '-last-fetched']);
+
+		if (now - lastFetched < 300000 && localStorage[context + '-data'] != null) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 chrome.runtime.onMessage.addListener(onMessage);
